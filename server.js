1b3b38c80474c58d04a6539ba9c5bce1411fe860
Tyler <182259162+cobb3r@users.noreply.github.com>
2024-11-20 20:31:44 +0000

    Added The Form and Basic CSS + Set Up Landing Page

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.static('frontend'))
 
+app.get('/success', function(req, res) {
+    try {
+        res.render('success.ejs')
+    }catch(error) {
+        res.status(500).end()
+    }
+})
+
 app.listen(port, function(error) {
     if (error) {
         console.log("Something Went Wrong", error)
